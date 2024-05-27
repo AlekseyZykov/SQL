c64424582cfa7db36b4e91ac54b3523d5a263fd3
AlekseyZykov <AlekseyZykov86@yandex.ru>
2024-05-27 15:46:15 +0300

    Update Rolling retention
    SQL

--- Rolling retention.sql
+++ Rolling retention.sql
@@ -1,3 +1,21 @@
+/* 
+PostgreSQL 16
+Данный запрос предназначен для расчета Rolling Retention 1, 3, 7, 14, 30, 60, и 90 дня в разрезе месячных когорт пользователей онлайн-школы за 2022 год. 
+Сервис характеризуется подписной моделью бизнеса.
+ 
+Таблица users содержит информацию о всех пользователях.
+Описание стобцов:
+id - уникальный идентификатор пользователя
+date_joined - дата и время регистрации
+
+Таблица userentry содержит информацию входе пользователя на платформу - когда, 
+в какое время и на какую страницу пользователь зашел первый раз за сутки.
+Описание стобцов:
+id - уникальный идентификатор пользователя
+entry_at - дата и время входа на платформу
+user_id - идентификатор пользователя (FK - Users)
+ */
+--Объединяем пользователей в когорты по месяцу регистрации в 2022 году
 with a as (
     select
         u.user_id,
@@ -10,3 +28,16 @@ with a as (
     on u.user_id = u2.id
     where to_char(u2.date_joined, 'YYYY') = '2022'
 )
+--Для каждой когорты рассчитываем Rolling Retention 1, 3, 7, 14, 30, 60, и 90 дня
+select
+    cohort,
+    round(count(distinct case when diff >= 0 then user_id end) * 100.0 / count(distinct case when diff >= 0 then user_id end), 2) as "0 (%)",
+    round(count(distinct case when diff >= 1 then user_id end) * 100.0 / count(distinct case when diff >= 0 then user_id end), 2) as "1 (%)",
+    round(count(distinct case when diff >= 3 then user_id end) * 100.0 / count(distinct case when diff >= 0 then user_id end), 2) as "3 (%)",
+    round(count(distinct case when diff >= 7 then user_id end) * 100.0 / count(distinct case when diff >= 0 then user_id end), 2) as "7 (%)",
+    round(count(distinct case when diff >= 14 then user_id end) * 100.0 / count(distinct case when diff >= 0 then user_id end), 2) as "14 (%)",
+    round(count(distinct case when diff >= 30 then user_id end) * 100.0 / count(distinct case when diff >= 0 then user_id end), 2) as "30 (%)",
+    round(count(distinct case when diff >= 60 then user_id end) * 100.0 / count(distinct case when diff >= 0 then user_id end), 2) as "60 (%)",
+    round(count(distinct case when diff >= 90 then user_id end) * 100.0 / count(distinct case when diff >= 0 then user_id end), 2) as "90 (%)"
+from a
+group by cohort
